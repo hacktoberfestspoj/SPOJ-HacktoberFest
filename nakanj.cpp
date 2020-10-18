0f473b2646a70ce305f07e998cec95266695aa2c
Nikhil Anand <46108032+respawner8@users.noreply.github.com>
2020-10-18 21:00:38 +0530

    adding NAKANJ.cpp

---
+++ nakanj.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+#define pb push_back
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+		{
+						
+		}	
+		
+	return 0;
+}
+
+
+
